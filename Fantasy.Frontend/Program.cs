3f296a9626d20f29c9760158469be37352c707df
Luis Núñez <luisalbertonu@gmail.com>
2024-09-28 15:02:48 -0300

    Video 9

--- Fantasy.Frontend/Program.cs
+++ Fantasy.Frontend/Program.cs
@@ -1,4 +1,5 @@
-using Fantasy._Frontend;
+using Fantasy.Frontend;
+using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7170") });
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddLocalization();
 await builder.Build().RunAsync();
